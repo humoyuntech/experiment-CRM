7e8aef5db796afd5d9aa30b9fc62958ff41af893
Rustamov Khumoyun <122399191+humoyuntech@users.noreply.github.com>
2024-07-26 23:17:35 +0500

    three

--- src/components/Sidebar/style.js
+++ src/components/Sidebar/style.js
@@ -29,67 +29,61 @@ export const SidebarList = styled.ul`
     border-bottom: 1px solid #253E5F;
     padding-bottom: 10px;
 `
-export const SidebarListLi = styled.li`
-    width: 100%;
-    height: 44px;
-    padding: 5px;
-    display: flex;
-    justify-content:  space-around;
-    align-items: center;
-    transition: all .8s ease;
-    
-    /* border: 1px solid white; */
-p{
-    width: 60%;
-}
 
-span {
+const commonLi = `
+        width: 100%;
+        height: 44px;
+        padding: 5px;
+        display: flex;
+        justify-content:  space-around;
+        align-items: center;
+        transition: all .8s ease;
+`
+
+const commonSpan = `
     display: flex;
     justify-content:  center;
     align-items: center;
     width: 20px;
     height: 20px;
+`
 
+const commonHover = `
+        cursor: pointer;
+        background-color: #253E5F;
+        color: #1890FF;
+`
+
+export const SidebarListLi = styled.li`
+   ${commonLi}
+    /* border: 1px solid white; */
+p {
+    width: 60%;
+}
+span {
+    ${commonSpan}
     /* outline: 1px solid red; */
 }
 
 &:hover {
-    cursor: pointer;
-    background-color: #253E5F;
-    color: #1890FF;
-
+   ${commonHover};
 }
-
 `
 
 export const Settings = styled.div`
-    width: 100%;
-    height: 44px;
-    padding: 5px;
+   ${commonLi}
     margin-top: 20px;
-    display: flex;
-    justify-content:  space-around;
-    align-items: center;
-    transition: all .8s ease;
 
 p{
     width: 60%;
 }
 
 span {
-    display: flex;
-    justify-content:  center;
-    align-items: center;
-    width: 20px;
-    height: 20px;
-
+    ${commonSpan}
     /* outline: 1px solid red; */
 }
 
 &:hover {
-    cursor: pointer;
-    background-color: #253E5F;
-    color: #1890FF;
+    ${commonHover};
 }
-
 `
